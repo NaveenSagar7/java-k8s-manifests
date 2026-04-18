250afed5033917449b490dc0b4e908fcd6bc6b1b
NaveenSagar7 <mandhadisagar3023@gmail.com>
2026-04-18 15:27:37 +0530

    made changes to script

--- scripts/update_image.sh
+++ scripts/update_image.sh
@@ -2,12 +2,12 @@
 
 VERSION=$1
 
-sed -i "s|image:.*|image: naveen352/java-demo-argocd:$VERSION|g" ../deploy.yaml
+sed -i "s|image:.*|image: naveen352/java-demo-argocd:$VERSION|g" deploy.yaml
 
-git config --local user.email "mandhadisagar3023@gmail.com"
-git config --local user.name "NaveenSagar7"
+#git config --local user.email "mandhadisagar3023@gmail.com"
+#git config --local user.name "NaveenSagar7"
 
-git add ../deploy.yaml
+git add deploy.yaml
 
 git commit -m "update image version $VERSION [skip ci]"
 
